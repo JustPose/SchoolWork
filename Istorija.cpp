bc3046575ea46e2b30f758f2d76743f8fa3f63c0
domantasrutkauskas <domantasrutkauskas0@gmail.com>
2021-05-20 14:32:11 +0300

    haha

---
+++ Istorija.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+int informacija()
+{
+
+    string vardai[4] = {'senelis', 'mama', 'tetis', 'mociute'};
+    string jungtukai[3] = {'ir', 'su', 'kartu'};
+    string budvardziai[3] = {'grazus', 'protingas', 'greitas'};
+    string draugai[3] = {'draugu', 'dukra', 'sunus'};
+}
+
+int main()
+{
+    cout << vardai[1];
+}
