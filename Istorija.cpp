34d701c9ca0d0749f1ce4786d422d181a998ebc9
JustPose <80042878+JustPose@users.noreply.github.com>
2021-05-27 15:19:25 +0300

    Add files via upload

--- Istorija.cpp
+++ Istorija.cpp
@@ -1,18 +1,31 @@
-#include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 #include <string>
 
 using std::cin;
 using std::cout;
+using std::endl;
 using std::string;
 
-string vardai[4] = {'senelis', 'mama', 'tetis', 'mociute'};
-string jungtukai[3] = {'ir', 'su', 'kartu'};
-string budvardziai[3] = {'grazus', 'protingas', 'greitas'};
-string draugai[3] = {'draugu', 'dukra', 'sunus'};
+string svardai[4] = {"senelis ", "mama ", "tetis ", "mociute "};
+string sjungtukai[3] = {"ir ", "su ", "kartu "};
+string sbudvardziai[3] = {"grazus ", "protingas ", "greitas "};
+string sdraugai[3] = {"draugu ", "dukra ", "sunus "};
 
-int main()
-{
-    cout << vardai[1];
+int ivardai = rand() % 4;
+int ijungtukai = rand() % 3;
+int ibudvardziai = rand() % 3;
+int idraugai = rand() % 3;
+
+int main() {
+  srand(time(NULL));
+
+  int ivardai = rand() % 4;
+  int ijungtukai = rand() % 3;
+  int ibudvardziai = rand() % 3;
+  int idraugai = rand() % 3;
+
+  cout << svardai[ivardai] << sjungtukai[ijungtukai] << sdraugai[idraugai]
+       << sbudvardziai[ibudvardziai] << endl;
 }
